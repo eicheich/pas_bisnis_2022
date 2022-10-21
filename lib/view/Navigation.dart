49866ed60a4adb85bba118af81af5ff43af21b1d
WilliAndHan <william.a.hanantha@gmail.com>
2022-10-21 19:58:45 +0700

    Feat [WA] bottom_bar

--- lib/view/Navigation.dart
+++ lib/view/Navigation.dart
@@ -1,22 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:pas_bisnis_2022/view/Home.dart';
 
-class Navigation extends StatefulWidget {
-  const Navigation({Key? key}) : super(key: key);
+void main() => runApp(const Navigation());
+
+class Navigation extends StatelessWidget {
+  const Navigation({super.key});
+
+  static const String _title = 'Flutter Code Sample';
 
   @override
-  State<Navigation> createState() => _NavigationState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
 }
 
-class _NavigationState extends State<Navigation> {
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Home(),
+    Text(
+      'Index 1: Shop',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: History',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Profile',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Container(
-        child: Row(
-          children: [Text("ambatukam")],
-        ),
-      )),
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Shop',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'History',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color(0xFF1E5128),
+        onTap: _onItemTapped,
+        selectedFontSize: 15,
+        unselectedFontSize: 15,
+        unselectedItemColor: Color(0xFF2B2D42),
+        unselectedLabelStyle: TextStyle(color: Color(0xFF2B2D42)),
+      ),
     );
   }
 }
