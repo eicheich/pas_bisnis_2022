3fa0d8b144123f969944dbeebb819ab871e0ea79
WilliAndHan <william.a.hanantha@gmail.com>
2022-10-24 11:08:36 +0700

    Style [WA] bottom_bar

--- lib/view/Navigation.dart
+++ lib/view/Navigation.dart
@@ -1,52 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:pas_bisnis_2022/shared/hexConvert.dart';
+import 'package:pas_bisnis_2022/view/History.dart';
 import 'package:pas_bisnis_2022/view/Home.dart';
+import 'package:pas_bisnis_2022/view/Notif.dart';
+import 'package:pas_bisnis_2022/view/Profile.dart';
 
-void main() => runApp(const Navigation());
-
-class Navigation extends StatelessWidget {
-  const Navigation({super.key});
-
-  static const String _title = 'Flutter Code Sample';
+class Navigation extends StatefulWidget {
+  const Navigation({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: MyStatefulWidget(),
-    );
-  }
+  State<Navigation> createState() => _NavigationState();
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({super.key});
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+class _NavigationState extends State<Navigation> {
+  int _selectedNavbar = 0;
   static const List<Widget> _widgetOptions = <Widget>[
     Home(),
-    Text(
-      'Index 1: Shop',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: History',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: Profile',
-      style: optionStyle,
-    ),
+    Notif(),
+    History(),
+    Profile(),
   ];
 
-  void _onItemTapped(int index) {
+  void _changeSelectedNavBar(int index) {
     setState(() {
-      _selectedIndex = index;
+      _selectedNavbar = index;
     });
   }
 
@@ -54,34 +31,59 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+        child: _widgetOptions.elementAt(_selectedNavbar),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            icon: ImageIcon(
+              AssetImage('assets/images/home.png'),
+            ),
+            activeIcon: ImageIcon(
+              AssetImage('assets/images/home.png'),
+            ),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            icon: ImageIcon(
+              AssetImage(
+                'assets/images/shop.png',
+              ),
+            ),
+            activeIcon: ImageIcon(
+              AssetImage('assets/images/shop.png'),
+            ),
             label: 'Shop',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
+            icon: ImageIcon(
+              AssetImage('assets/images/history.png'),
+            ),
+            activeIcon: ImageIcon(
+              AssetImage('assets/images/history.png'),
+            ),
             label: 'History',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
+            icon: ImageIcon(
+              AssetImage('assets/images/user.png'),
+            ),
+            activeIcon: ImageIcon(
+              AssetImage('assets/images/user.png'),
+            ),
             label: 'Profile',
           ),
         ],
-        currentIndex: _selectedIndex,
+        currentIndex: _selectedNavbar,
+        iconSize: 24,
         selectedItemColor: Color(0xFF1E5128),
-        onTap: _onItemTapped,
-        selectedFontSize: 15,
-        unselectedFontSize: 15,
         unselectedItemColor: Color(0xFF2B2D42),
-        unselectedLabelStyle: TextStyle(color: Color(0xFF2B2D42)),
+        showUnselectedLabels: true,
+        selectedLabelStyle: TextStyle(
+            fontFamily: "Lexend", fontWeight: FontWeight.w600, fontSize: 12),
+        unselectedLabelStyle: TextStyle(
+            fontFamily: "Lexend", fontWeight: FontWeight.w600, fontSize: 12),
+        onTap: _changeSelectedNavBar,
       ),
     );
   }
