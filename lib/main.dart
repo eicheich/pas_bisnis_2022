d872a5085d9631f81f469eafb293e4bf3df57928
WilliAndHan <william.a.hanantha@gmail.com>
2022-10-24 16:18:46 +0700

    Style [WA] home_screen

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
+      debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
     );
   }
